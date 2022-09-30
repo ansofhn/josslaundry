959a44fd725c0752f6286f05d385c43c9bfa2e41
rahmat-19 <rahmatjr023@gmail.com>
2022-09-30 10:43:20 +0700

    Rahmat -ADD PAGE ABOUT US (SLIDE-ONE)

--- pages/about.js
+++ pages/about.js
@@ -1,9 +1,25 @@
 import React from 'react'
+import Head from "next/head";
+import HomeLayout from "../layouts/HomeLayout";
+import Footer from "../components/Footer";
+import Navbar from "../components/Navbar";
+import SlideOne from '../components/aboutComponent/SlideOne';
 
 export default function about() {
   return (
-    <div>
-      
-    </div>
+    <>
+      <Head>
+        <title>About Us | Joss Laundry</title>
+      </Head>
+      <Navbar />
+
+
+      <SlideOne />
+
+
+      <Footer />
+    </>
   )
 }
+
+about.getLayout = (page) => <HomeLayout children={page} />;
