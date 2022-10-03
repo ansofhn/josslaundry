8984b58899e63d3722c27aaa84d06e0b13a60a0c
Barita Davitya <bdavitya@gmail.com>
2022-10-03 17:54:38 +0700

    banner

--- components/Hero.js
+++ components/Hero.js
@@ -5,16 +5,13 @@ import Banner from "../public/assets/logo-banner.png";
 export default function Hero() {
   return (
     <>
-      <div className="w-full bg-white">
+      <div className="w-full bg-gradient-to-tr from-orange-500 to-yellow-500">
         <div className="container">
-          <div className="grid items-center justify-center grid-cols-2 mx-5 my-24 bg-white">
-            <div className="px-20 py-20">
-              <h1 className="text-5xl font-bold font-poppins text-darkgreen 2xl:mr-20">
+          <div className="grid items-center justify-center grid-cols-2 mx-5 my-16">
+            <div className="px-20 py-15">
+              <h1 className="text-5xl font-bold font-poppins text-darkgreen 2xl:mr-20 ">
                 Bebaskan Indonesia Dari Jemuran
               </h1>
-              <p className="mt-4 mb-6 text-lg leading-relaxed font-poppins text-darkgreen 2xl:mr-28">
-                Percayakan pada kami, cucian anda bersih dan wangi.
-              </p>
             </div>
             <div className="mx-auto">
               <Image src={Banner} />
