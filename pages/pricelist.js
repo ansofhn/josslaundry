0f0aa5a09a7fc394a8770be62bdca8ece77d529d
Ansof Habibunnadjar <ansofhn5@gmail.com>
2022-10-18 19:30:11 +0700

    fix: pricelist code for redeploy

--- pages/pricelist.js
+++ pages/pricelist.js
@@ -2,8 +2,6 @@ import Head from "next/head";
 import React from "react";
 import Footer_v2 from "../components/Footer_v2";
 import Navbar_v2 from "../components/Navbar_v2";
-import Card from "../components/pricelistComponent/Card";
-import Card_v2 from "../components/pricelistComponent/Card_v2";
 import Pricing from "../components/Pricing";
 
 export default function pricelist() {
@@ -13,11 +11,11 @@ export default function pricelist() {
         <title>Joss Laundry - Daftar Harga</title>
       </Head>
       <Navbar_v2 />
-      <div className="w-full bg-gradient-to-tr from-orange-500 to-yellow-500 mt-16">
+      <div className="w-full mt-16 bg-gradient-to-tr from-orange-500 to-yellow-500">
         <div className="container">
           <div className="flex items-center justify-center py-0 m-0 mx-5 md:my-14">
             <div className="py-20 text-white">
-              <h1 className="font-bold uppercase text-3xl lg:text-4xl xl:text-5xl">
+              <h1 className="text-3xl font-bold uppercase lg:text-4xl xl:text-5xl">
                 Daftar Harga
               </h1>
             </div>
@@ -26,17 +24,24 @@ export default function pricelist() {
       </div>
       <Pricing />
       <div className="container">
-        <div className="mt-14 mb-4 font-bold text-center text-2xl xl:text-3xl 2xl:text-4xl text-maroon">
+        <div className="mb-4 text-2xl font-bold text-center mt-14 xl:text-3xl 2xl:text-4xl text-maroon">
           Harga Lebih Lengkap
         </div>
-        <div className="mb-20 text-base lg:text-lg font-light text-center text-textColor">
+        <div className="mb-20 text-base font-light text-center lg:text-lg text-textColor">
           Kami secara konsisten berupaya memberikan yang terbaik untuk kepuasan
           pelanggan.
         </div>
-        <div className="grid md:grid-cols-2 gap-6 mx-4 md:mx-8 lg:mx-24 lg:my-20">
-          <div className="text-lg font-bold text-maroon order-1">Paket Laundry Kiloan</div>
-          <div className="text-lg font-bold text-maroon order-3 md:order-2" id="pakaian">Pakaian Satuan</div>
-          <div className="p-6 border-t-2 rounded-br-md rounded-bl-md bg-gray-50 border-amber order-2 md:order-3">
+        <div className="grid gap-6 mx-4 md:grid-cols-2 md:mx-8 lg:mx-24 lg:my-20">
+          <div className="order-1 text-lg font-bold text-maroon">
+            Paket Laundry Kiloan
+          </div>
+          <div
+            className="order-3 text-lg font-bold text-maroon md:order-2"
+            id="pakaian"
+          >
+            Pakaian Satuan
+          </div>
+          <div className="order-2 p-6 border-t-2 rounded-br-md rounded-bl-md bg-gray-50 border-amber md:order-3">
             <h2 className="mt-2 mb-2 font-medium">Reguler (2 Hari)</h2>
             <div className="flex items-center justify-between text-sm font-light text-darkgreen">
               <div>Cuci Kering Lipat 3 Kg</div>
@@ -65,7 +70,7 @@ export default function pricelist() {
               <div>50,000</div>
             </div>
           </div>
-          <div className="p-6 border-t-2 rounded-br-md rounded-bl-md bg-gray-50 border-amber order-4">
+          <div className="order-4 p-6 border-t-2 rounded-br-md rounded-bl-md bg-gray-50 border-amber">
             <h2 className="mt-2 mb-2 font-medium">Baju</h2>
             <div className="flex items-center justify-between text-sm font-light text-darkgreen">
               <div>Baju Kaos</div>
@@ -94,9 +99,16 @@ export default function pricelist() {
               <div>21,000</div>
             </div>
           </div>
-          <div className="text-lg font-bold text-maroon order-5" id="house-care">House Care</div>
-          <div className="text-lg font-bold text-maroon order-7 md:order-6">Lainnya</div>
-          <div className="p-6 border-t-2 rounded-br-md rounded-bl-md bg-gray-50 border-amber order-6 md:order-7">
+          <div
+            className="order-5 text-lg font-bold text-maroon"
+            id="house-care"
+          >
+            House Care
+          </div>
+          <div className="order-7 text-lg font-bold text-maroon md:order-6">
+            Lainnya
+          </div>
+          <div className="order-6 p-6 border-t-2 rounded-br-md rounded-bl-md bg-gray-50 border-amber md:order-7">
             <h2 className="mt-2 mb-2 font-medium">Keset</h2>
             <div className="flex items-center justify-between text-sm font-light text-darkgreen">
               <div>Keset Kecil</div>
@@ -137,8 +149,10 @@ export default function pricelist() {
               <div>50,000</div>
             </div>
           </div>
-          <div className="p-6 border-t-2 rounded-br-md rounded-bl-md bg-gray-50 border-amber order-8">
-            <h2 className="mt-2 mb-2 font-medium" id="helm">Helm</h2>
+          <div className="order-8 p-6 border-t-2 rounded-br-md rounded-bl-md bg-gray-50 border-amber">
+            <h2 className="mt-2 mb-2 font-medium" id="helm">
+              Helm
+            </h2>
             <div className="flex items-center justify-between text-sm font-light text-darkgreen">
               <div>Helm Biasa</div>
               <div>25,000</div>
@@ -147,7 +161,9 @@ export default function pricelist() {
               <div>Helm Fullface</div>
               <div>30,000</div>
             </div>
-            <h2 className="mt-4 mb-2 font-medium" id="boneka">Boneka</h2>
+            <h2 className="mt-4 mb-2 font-medium" id="boneka">
+              Boneka
+            </h2>
             <div className="flex items-center justify-between text-sm font-light text-darkgreen">
               <div>Boneka Kecil</div>
               <div>15,000</div>
@@ -160,7 +176,9 @@ export default function pricelist() {
               <div>Boneka Besar</div>
               <div>70,000</div>
             </div>
-            <h2 className="mt-4 mb-2 font-medium" id="baby-care">Baby Care</h2>
+            <h2 className="mt-4 mb-2 font-medium" id="baby-care">
+              Baby Care
+            </h2>
             <div className="flex items-center justify-between text-sm font-light text-darkgreen">
               <div>Gendongan Bayi Kecil</div>
               <div>20,000</div>
