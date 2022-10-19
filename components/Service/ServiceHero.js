d0c2669d8f7d073a8b428f5deec3dd95a63b58d3
Ansof Habibunnadjar <ansofhn5@gmail.com>
2022-10-20 00:06:11 +0700

    fix: about n service banner

--- components/Service/ServiceHero.js
+++ components/Service/ServiceHero.js
@@ -1,21 +1,19 @@
 import React from "react";
 import Image from "next/image";
-import Link from "next/link";
-import Button from "../Button";
-import { MdNavigateNext } from "react-icons/md";
-import layanankami from '../../public/assets/layanan-kami-2.png';
-// import layanankami from '../../public/assets/vektor-hero-layanan-kami.svg'
+import layanankami from "../../public/assets/layanan-kami-2.png";
 
 export default function ServiceHero() {
   return (
     <section className="px-3 pb-5 pt-[110px] bg-gradient-to-tr from-orange-500 to-yellow-500">
       <div className="container">
-        <div className="grid lg:grid-cols-2 items-center justify-items-center lg:gap-5">
-          <div className="flex flex-col justify-center items-center">
-            <p className="flex text-4xl sm:text-5xl lg:text-6xl font-bold text-white text-center tracking-normal">Layanan Kami</p>
+        <div className="grid items-center pb-10 lg:grid-cols-2 justify-items-center lg:gap-5">
+          <div className="flex flex-col items-center justify-center">
+            <p className="flex text-4xl font-bold tracking-normal text-center text-white sm:text-5xl lg:text-6xl">
+              Layanan Kami
+            </p>
           </div>
-          <div className="grid place-items-center scale-75 lg:scale-100">
-            <Image src={layanankami}/>
+          <div className="grid scale-75 place-items-center lg:scale-100">
+            <Image src={layanankami} />
           </div>
         </div>
       </div>
--- pages/about.js
+++ pages/about.js
@@ -17,14 +17,16 @@ export default function about() {
       <Navbar_v2 />
       <section className="px-3 pb-5 pt-[70px] md:pt-[110px] bg-gradient-to-tr from-orange-500 to-yellow-500">
         <div className="container">
-          <div className="grid lg:grid-cols-2 items-center justify-items-center lg:gap-5">
-            <div className="grid place-items-center scale-75 lg:scale-100">
-              <Image src={aboutus}/>
+          <div className="grid items-center pb-10 lg:grid-cols-2 justify-items-center lg:gap-5">
+            <div className="grid scale-75 place-items-center lg:scale-100">
+              <Image src={aboutus} />
             </div>
-            <div className="flex flex-col justify-center items-center">
-              <p className="flex text-4xl sm:text-5xl lg:text-6xl font-bold text-white text-center tracking-normal">Tentang Kami</p>
+            <div className="flex flex-col items-center justify-center">
+              <p className="flex text-4xl font-bold tracking-normal text-center text-white sm:text-5xl lg:text-6xl">
+                Tentang Kami
+              </p>
             </div>
-          </div>  
+          </div>
         </div>
       </section>
       <About_v3 />
