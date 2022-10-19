b3a797edc3077093ef2dea0c182a803ea3c91707
Ansof Habibunnadjar <ansofhn5@gmail.com>
2022-10-19 15:13:37 +0700

    fix: cleaning code n change text color based on color palette

--- pages/service.js
+++ pages/service.js
@@ -8,8 +8,6 @@ import ScrollToTop from "../components/ScrollToTop";
 import ShoesCare from "../components/Service/ShoesCare";
 import LayananLainnya from "../components/Service/LayananLainnya";
 import Head from "next/head";
-import Image from "next/image";
-import layanankami from '../public/assets/layanan-kami.png';
 import ServiceHero from "../components/Service/ServiceHero";
 
 export default function service() {
@@ -19,20 +17,6 @@ export default function service() {
         <title>Joss Laundry - Layanan</title>
       </Head>
       <Navbar_v2 />
-      {/* <div className="w-full bg-gradient-to-tr from-nileblue to-middle">
-        <div className="container">
-          <div className="grid items-center justify-center grid-cols-2 py-0 m-0 mx-5 md:my-12 2xl:py-10">
-            <div className="px-20 text-white">
-              <h1 className="font-bold md:text-3xl lg:text-4xl xl:text-5xl 2xl:text-6xl">
-                Layanan Kami
-              </h1>
-            </div>
-            <div className="mx-auto mt-20 mb-10">
-              <Image src={layanankami} />
-            </div>
-          </div>
-        </div>
-      </div> */} 
       <ServiceHero />
       <HouseCare />
       <BabyCare />
