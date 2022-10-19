cfecee18829689047f88713d877f26188c127bcf
Barita Davitya <bdavitya@gmail.com>
2022-10-19 15:52:17 +0700

    fixx:about

--- pages/about.js
+++ pages/about.js
@@ -15,14 +15,16 @@ export default function about() {
         <title>Joss Laundry - Tentang</title>
       </Head>
       <Navbar_v2 />
-      <section className="px-3 bg-gradient-to-tr from-orange-500 to-yellow-500 lg:h-[555px]">
-        <div className="grid lg:grid-cols-2 items-center justify-items-center lg:gap-5">
-          <div className="grid place-items-center h-[22rem] sm:h-[25.5rem] pt-[8rem] sm:pt-0 sm:mt-[10rem] lg:mt-[14.5rem] xl:mt-[13.8rem]">
-            <Image src={aboutus} height={"540rem"} width={"540rem"}/>
-          </div>
-          <div className="flex flex-col justify-center items-center">
-            <p className="mb-6 sm:mb-10 lg:mb-0 flex text-4xl sm:text-5xl lg:text-6xl font-bold text-white text-center tracking-normal">Tentang Kami</p>
-          </div>
+      <section className="px-3 pb-5 pt-[70px] md:pt-[110px] bg-gradient-to-tr from-orange-500 to-yellow-500">
+        <div className="container">
+          <div className="grid lg:grid-cols-2 items-center justify-items-center lg:gap-5">
+            <div className="grid place-items-center scale-75 lg:scale-100">
+              <Image src={aboutus}/>
+            </div>
+            <div className="flex flex-col justify-center items-center">
+              <p className="flex text-4xl sm:text-5xl lg:text-6xl font-bold text-white text-center tracking-normal">Tentang Kami</p>
+            </div>
+          </div>  
         </div>
       </section>
       <About_v3 />
