2d860dad6babf012a7d94a9b6a46ad28c504715c
Ansof Habibunnadjar <ansofhn5@gmail.com>
2022-10-19 23:44:18 +0700

    fix: accordion on about us page

--- components/About_v3.js
+++ components/About_v3.js
@@ -1,9 +1,16 @@
 import Image from "next/image";
-import React, { Component } from "react";
+import React, { Component, useState } from "react";
 import profile from "../public/assets/brand.png";
 import faq from "../public/assets/faq.png";
 
 const About_v3 = () => {
+  const [showQ1, setShowQ1] = useState(false);
+  const [showQ2, setShowQ2] = useState(false);
+  const [showQ3, setShowQ3] = useState(false);
+  const [showQ4, setShowQ4] = useState(false);
+  const [showQ5, setShowQ5] = useState(false);
+  const [showQ6, setShowQ6] = useState(false);
+
   return (
     <div className="w-full bg-white">
       <div className="container text-darkgreen">
@@ -67,7 +74,7 @@ const About_v3 = () => {
               <div className="mx-10 md:hidden sm:mx-20 md:mx-0 md:mt-24 lg:mt-14">
                 <Image src={faq} />
               </div>
-              <div className="mb-4 font-semibold text-left 2xl:mt-10 sm:text-2xl md:text-xl lg:text-2xl xl:text-3xl 2xl:text-4xl">
+              <div className="my-4 font-semibold text-left 2xl:mt-10 sm:text-2xl md:text-xl lg:text-2xl xl:text-3xl 2xl:text-4xl">
                 FAQ
               </div>
               <hr
@@ -77,77 +84,264 @@ const About_v3 = () => {
                 }}
               />
               <br />
-              <div className="mb-4 dropdown-button">
-                <div className="mb-2 font-semibold dropdown-btn sm:text-lg lg:text-xl">
-                  Apa, yang kami lakukan?
+              <div>
+                {/* Question 1 */}
+                <h2>
+                  <button
+                    type="button"
+                    className="flex items-center justify-between w-full py-3 font-semibold text-left bg-white text-darkgreen"
+                    onClick={() => {
+                      setShowQ1(!showQ1);
+                      setShowQ2(false);
+                      setShowQ3(false);
+                      setShowQ4(false);
+                      setShowQ5(false);
+                      setShowQ6(false);
+                    }}
+                  >
+                    <span>Apa yang kami lakukan?</span>
+                    <svg
+                      className={`w-6 h-6 ${
+                        showQ1 ? "rotate-180" : ""
+                      } shrink-0`}
+                      fill="currentColor"
+                      viewBox="0 0 20 20"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <path
+                        fillRule="evenodd"
+                        d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
+                        clipRule="evenodd"
+                      />
+                    </svg>
+                  </button>
+                </h2>
+                <div className={`${showQ1 ? "visible" : "hidden"}`}>
+                  <div className="py-3 text-sm font-light">
+                    <p className="mb-2 text-darkgreen/80">
+                      Kami selalu berupaya memberikan kepuasan kepada para
+                      pelanggan terhadap hasil dari jasa yang kami tawarkan
+                      dengan tenaga-tenaga yang sudah terlatih dan kompeten
+                      sehingga menghasilkan pekerjaan yang baik, dan cepat namun
+                      tetap dengan harga yang terjangkau.
+                    </p>
+                  </div>
                 </div>
-                <div className="pl-4 text-xs text-justify dropdown-container sm:text-sm lg:text-base lg:pl-6">
-                  Kami selalu berupaya memberikan kepuasan kepada para pelanggan
-                  terhadap hasil dari jasa yang kami tawarkan dengan
-                  tenaga-tenaga yang sudah terlatih dan kompeten sehingga
-                  menghasilkan pekerjaan yang baik, dan cepat namun tetap dengan
-                  harga yang terjangkau.
+
+                {/* Question 2 */}
+                <h2>
+                  <button
+                    type="button"
+                    className="flex items-center justify-between w-full py-3 font-semibold text-left bg-white text-darkgreen"
+                    onClick={() => {
+                      setShowQ2(!showQ2);
+                      setShowQ1(false);
+                      setShowQ3(false);
+                      setShowQ4(false);
+                      setShowQ5(false);
+                      setShowQ6(false);
+                    }}
+                  >
+                    <span>Dimana lokasi kami?</span>
+                    <svg
+                      className={`w-6 h-6 ${
+                        showQ2 ? "rotate-180" : ""
+                      } shrink-0`}
+                      fill="currentColor"
+                      viewBox="0 0 20 20"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <path
+                        fillRule="evenodd"
+                        d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
+                        clipRule="evenodd"
+                      />
+                    </svg>
+                  </button>
+                </h2>
+                <div className={`${showQ2 ? "visible" : "hidden"}`}>
+                  <div className="py-3 text-sm font-light">
+                    <p className="mb-2 text-darkgreen/80">
+                      Kami berlokasi di Jalan Sukabirus Nomor 88B, Citeureup,
+                      Kabupaten Bandung, Jawa Barat
+                    </p>
+                  </div>
                 </div>
-              </div>
-              <div className="mb-4 dropdown-button">
-                <div className="mb-2 font-semibold dropdown-btn sm:text-lg lg:text-xl">
-                  Dimana, lokasi kami?
+
+                {/* Question 3 */}
+                <h2>
+                  <button
+                    type="button"
+                    className="flex items-center justify-between w-full py-3 font-semibold text-left bg-white text-darkgreen"
+                    onClick={() => {
+                      setShowQ3(!showQ3);
+                      setShowQ1(false);
+                      setShowQ2(false);
+                      setShowQ4(false);
+                      setShowQ5(false);
+                      setShowQ6(false);
+                    }}
+                  >
+                    <span>Kapan jam operasional kami?</span>
+                    <svg
+                      className={`w-6 h-6 ${
+                        showQ3 ? "rotate-180" : ""
+                      } shrink-0`}
+                      fill="currentColor"
+                      viewBox="0 0 20 20"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <path
+                        fillRule="evenodd"
+                        d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
+                        clipRule="evenodd"
+                      />
+                    </svg>
+                  </button>
+                </h2>
+                <div className={`${showQ3 ? "visible" : "hidden"}`}>
+                  <div className="py-3 text-sm font-light">
+                    <p className="mb-2 text-darkgreen/80">
+                      Kami beroperasi mulai pukul 09.00 WIB hingga 22.00 WIB
+                    </p>
+                  </div>
                 </div>
-                <div className="pl-4 text-xs text-justify dropdown-container sm:text-sm lg:text-base lg:pl-6">
-                  Kami berlokasi di Jalan Sukabirus Nomor 88B, Citeureup,
-                  Kabupaten Bandung, Jawa Barat
+
+                {/* Question 4 */}
+                <h2>
+                  <button
+                    type="button"
+                    className="flex items-center justify-between w-full py-3 font-semibold text-left bg-white text-darkgreen"
+                    onClick={() => {
+                      setShowQ4(!showQ4);
+                      setShowQ1(false);
+                      setShowQ2(false);
+                      setShowQ3(false);
+                      setShowQ5(false);
+                      setShowQ6(false);
+                    }}
+                  >
+                    <span>Siapa kami?</span>
+                    <svg
+                      className={`w-6 h-6 ${
+                        showQ4 ? "rotate-180" : ""
+                      } shrink-0`}
+                      fill="currentColor"
+                      viewBox="0 0 20 20"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <path
+                        fillRule="evenodd"
+                        d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
+                        clipRule="evenodd"
+                      />
+                    </svg>
+                  </button>
+                </h2>
+                <div className={`${showQ4 ? "visible" : "hidden"}`}>
+                  <div className="py-3 text-sm font-light">
+                    <p className="mb-2 text-darkgreen/80">
+                      Kami,{" "}
+                      <span className="font-semibold text-maroon">
+                        Joss Laundry Telkom University
+                      </span>{" "}
+                      merupakan salah satu cabang resmi dari{" "}
+                      <span className="font-semibold">Joss Laundry</span> di
+                      Indonesia. Berdiri pertamakali di Rawamangun pada tahun
+                      2020 oleh PT Berkah Bersih Semesta,{" "}
+                      <span className="font-semibold">Joss Laundry</span>{" "}
+                      menjadi tempat laundry yang terpecaya dan dikenal dengan
+                      harga terjangkau namun tetap dengan kualitas yang mewah
+                    </p>
+                  </div>
                 </div>
-              </div>
-              <div className="mb-4 dropdown-button">
-                <div className="mb-2 font-semibold dropdown-btn sm:text-lg lg:text-xl">
-                  Kapan, jam operasional kami?
+
+                {/* Question 5 */}
+                <h2>
+                  <button
+                    type="button"
+                    className="flex items-center justify-between w-full py-3 font-semibold text-left bg-white text-darkgreen"
+                    onClick={() => {
+                      setShowQ5(!showQ5);
+                      setShowQ1(false);
+                      setShowQ2(false);
+                      setShowQ3(false);
+                      setShowQ4(false);
+                      setShowQ6(false);
+                    }}
+                  >
+                    <span>Mengapa harus memilih kami?</span>
+                    <svg
+                      className={`w-6 h-6 ${
+                        showQ5 ? "rotate-180" : ""
+                      } shrink-0`}
+                      fill="currentColor"
+                      viewBox="0 0 20 20"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <path
+                        fillRule="evenodd"
+                        d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
+                        clipRule="evenodd"
+                      />
+                    </svg>
+                  </button>
+                </h2>
+                <div className={`${showQ5 ? "visible" : "hidden"}`}>
+                  <div className="py-3 text-sm font-light">
+                    <p className="mb-2 text-darkgreen/80">
+                      Selain memiliki harga yang sangat terjangkau dan layanan
+                      yang cepat, kami memiliki jasa Pick Up and Delivery untuk
+                      para pelanggan yang sulit mengantarkan cuciannya kepada
+                      kami. Bukan hanya itu, kami juga memiliki banyak promo
+                      yang bisa dilihat di sosial media milik kami
+                    </p>
+                  </div>
                 </div>
-                <div className="pl-4 text-xs text-justify dropdown-container sm:text-sm lg:text-base lg:pl-6">
-                  Kami beroperasi mulai pukul 09.00 WIB hingga 22.00 WIB
-                </div>
-              </div>
-              <div className="mb-4 dropdown-button">
-                <div className="mb-2 font-semibold dropdown-btn sm:text-lg lg:text-xl">
-                  Siapa, kami?
-                </div>
-                <div className="pl-4 text-xs text-justify dropdown-container sm:text-sm lg:text-base lg:pl-6">
-                  Kami,{" "}
-                  <span className="font-semibold text-maroon">
-                    Joss Laundry Telkom University
-                  </span>{" "}
-                  merupakan salah satu cabang resmi dari{" "}
-                  <span className="font-semibold">Joss Laundry</span> di
-                  Indonesia. Berdiri pertamakali di Rawamangun pada tahun 2020
-                  oleh PT Berkah Bersih Semesta,{" "}
-                  <span className="font-semibold">Joss Laundry</span> menjadi
-                  tempat laundry yang terpecaya dan dikenal dengan harga
-                  terjangkau namun tetap dengan kualitas yang mewah
-                </div>
-              </div>
-              <div className="mb-4 dropdown-button">
-                <div className="mb-2 font-semibold dropdown-btn sm:text-lg lg:text-xl">
-                  Mengapa, harus memilih kami?
-                </div>
-                <div className="pl-4 text-xs text-justify dropdown-container sm:text-sm lg:text-base lg:pl-6">
-                  Selain memiliki harga yang sangat terjangkau dan layanan yang
-                  cepat, kami memiliki jasa Pick Up and Delivery untuk para
-                  pelanggan yang sulit mengantarkan cuciannya kepada kami. Bukan
-                  hanya itu, kami juga memiliki banyak promo yang bisa dilihat
-                  di sosial media milik kami
-                </div>
-              </div>
-              <div className="mb-4 dropdown-button">
-                <div className="mb-2 font-semibold dropdown-btn sm:text-lg lg:text-xl">
-                  Bagaimana, cara melakukan pemesanan?
-                </div>
-                <div className="pl-4 text-xs text-justify dropdown-container sm:text-sm lg:text-base lg:pl-6">
-                  Para pelanggan bisa melalukan pemesanan secara langsung ke
-                  outlet kami atau bisa menghubungi kami dengan menekan "Hubungi
-                  Kami" yang terletak di pojok kanan atas layar
+
+                {/* Question 6 */}
+                <h2>
+                  <button
+                    type="button"
+                    className="flex items-center justify-between w-full py-3 font-semibold text-left bg-white text-darkgreen"
+                    onClick={() => {
+                      setShowQ6(!showQ6);
+                      setShowQ1(false);
+                      setShowQ2(false);
+                      setShowQ3(false);
+                      setShowQ4(false);
+                      setShowQ5(false);
+                    }}
+                  >
+                    <span>Bagaimana cara melakukan pemesanan?</span>
+                    <svg
+                      className={`w-6 h-6 ${
+                        showQ6 ? "rotate-180" : ""
+                      } shrink-0`}
+                      fill="currentColor"
+                      viewBox="0 0 20 20"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <path
+                        fillRule="evenodd"
+                        d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
+                        clipRule="evenodd"
+                      />
+                    </svg>
+                  </button>
+                </h2>
+                <div className={`${showQ6 ? "visible" : "hidden"}`}>
+                  <div className="py-3 text-sm font-light">
+                    <p className="mb-2 text-darkgreen/80">
+                      Para pelanggan bisa melalukan pemesanan secara langsung ke
+                      outlet kami atau bisa menghubungi kami dengan menekan
+                      "Hubungi Kami" yang terletak di pojok kanan atas layar
+                    </p>
+                  </div>
                 </div>
               </div>
             </div>
-            <div className="hidden mt-32 md:inline md:mt-24 lg:mt-14 xl:mt-4">
+            <div className="hidden mt-32 md:inline md:mt-24 lg:mt-14 xl:mt-12">
               <Image src={faq} />
             </div>
           </div>
